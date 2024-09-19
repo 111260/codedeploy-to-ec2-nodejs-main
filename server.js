a2fe324ed597f4cca5a259791342f939b78d12f1
111260 <muhammadafshalsajid@gmail.com>
2024-09-19 19:28:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
 
-app.use(express.static(__dirname + '/public'))
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + '/public'));
 
+// Serve the main page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
-// Socket 
-const io = require('socket.io')(http)
+// Health check route
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
 
+// Socket.io setup
 io.on('connection', (socket) => {
-    console.log('Connected...')
+    console.log('Connected...');
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
-
-})
+        socket.broadcast.emit('message', msg);
+    });
+});
